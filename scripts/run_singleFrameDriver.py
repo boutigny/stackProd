b414a56af329e3962b144d92f8229897008bdcc7
Dominique Boutigny <boutigny@in2p3.fr>
2019-05-03 18:51:53 +0200

    rename run_batch_all.py to run_singleFrameDriver.py + bug corrections and improvements

--- scripts/run_singleFrameDriver.py
+++ scripts/run_singleFrameDriver.py
@@ -3,6 +3,7 @@
 import os
 import time
 import sys
+import getpass
 from optparse import OptionParser
 import sqlite3
 from sqlite3 import Error
@@ -23,25 +24,44 @@ def getFilterList(dbConn):
     list = [i[0] for i in rows]
     return list
 
-def submit(visitList, toWrite, configFile, filt) :
-    filename = "lists/" + filt + "/" + visitList + ".list"
-    if not os.path.isdir("lists/" + filt) :
-        os.makedirs("lists/" + filt)
-        
+def createList(dbConn, filt):
+    cur = dbConn.cursor()
+    cur.execute("SELECT visit from raw_visit WHERE filter like '"+filt+"%'")
+    rows = cur.fetchall()
+    list = [i[0] for i in rows]
+    return list
+
+def createIdFile(visitList, fileName):
+    fd = open(fileName,"w+")
+    for vis in visitList:
+        st = "--id visit=" + str(vis) + "\n"
+        fd.write(st)
+    fd.close()
+    return None
+
+
+def createSubmit(visitList, toWrite, configFile, filt, clobber, rerun, launch) :
+    filename = "scripts/" + filt + "/" + visitList + ".list"
+    if not os.path.isdir("scripts/" + filt) :
+        os.makedirs("scripts/" + filt)
+
     theFile = open(filename,"w+")
     for line in toWrite:
         print(line, file=theFile)
-    cmd = "processCcd.py input --output "+ outputDir +" @" + filename + " --configfile " + configFile + " --clobber-config -j 8 --clobber-versions --timeout 999999999"
+    cmd = "singleFrameDriver.py _parent/input --rerun "+ rerun +" @" + filename + " --configfile " + configFile + " --cores 8 --timeout 999999999"
+    if clobber:
+        cmd = cmd + " --clobber-config --clobber-versions"
     print(cmd)
-    
+
     cwd = os.getcwd()
     dirLog = cwd + "/log/" + filt
     if not os.path.isdir(dirLog) :
         os.makedirs(dirLog)
     log = dirLog + "/" + visitList + ".log"
     print(log)
-    qsub = "qsub -P P_lsst -q mc_long -pe multicores 8 -l sps=1 -j y -o "+ log + " <<EOF"
-    scriptName = "lists/" + filt + "/" + visitList + ".sh"
+    jobName = 'singleFrameDriver_' + filt
+    qsub = "qsub -N " + jobName + " -P P_lsst -q mc_long -pe multicores 8 -l sps=1 -j y -o "+ log + " <<EOF"
+    scriptName = "scripts/" + filt + "/" + visitList + ".sh"
     script = open(scriptName,"w")
     script.write(qsub + "\n")
     script.write("#!/usr/local/bin/bash\n")
@@ -51,39 +71,50 @@ def submit(visitList, toWrite, configFile, filt) :
     script.write("EOF" + "\n")
     script.close()
     os.system("chmod +x " + scriptName)
-    os.system("./"+scriptName)
-    time.sleep(1)
+    if launch:
+        os.system("./"+scriptName)
+        time.sleep(1)
 
 if __name__ == "__main__":
 
     parser = OptionParser(usage="usage: %prog [options] input - print jobs info before launching them",
                           version="%prog 1.0")
-    parser.add_option("-O", "--output", type="string", default="output", help="Output [%default]")
+    parser.add_option("-r", "--rerun", type="string", default=getpass.getuser(), help="rerun directory [%default]")
     parser.add_option("-F", "--filter", type="string", default="all", help="Filter [%default]")
     parser.add_option("-m", "--mod", type="int", default=10, help="Nbr. of visits per job [%default]")
     parser.add_option("-M", "--max", type="int", default=999, help="Max nbr of jobs to be submitted [%default]")
     parser.add_option("-c", "--config", type="string", default="myProcessCcd.py", help="Configuration file [%default]")
-    parser.add_option("-L", "--launch", type="int", default="0", help="Launch the jobs [%default]")
+    parser.add_option("-L", "--launch", action="store_true", help="Launch the jobs if set")
+    parser.add_option("--clobber", action="store_true", help="Clobber everything if set")
     (opts, args) = parser.parse_args()
 
+    if not os.path.islink("_parent"):
+        sys.exit('_parent link is missing')
+
     #take the list from makeVisitList.py
-    db = os.path.join(args[0],'registry.sqlite3')
+    db = os.path.join('_parent','input','registry.sqlite3')
     conn = createConnection(db)
     # First get the list of available filters in the registry
     filterList = getFilterList(conn)
+    print("The following filters are available in the registry: ",filterList)
 
     filt = opts.filter
 
     if filt != "all":
-        if filt not in filterList :
-            print("filter %s not in registry"%filt)
-            exit(99)
- 
+        if filt not in filterList:
+            sys.exit("filter %s not in registry"%filt)
+        visitList = createList(conn, filt)
+        createIdFile(visitList, filt + ".list")
+    else:
+        for f in filterList:
+            visitList = createList(conn, f)
+            createIdFile(visitList, f + ".list")
+
     modularity = opts.mod
     maxVisit = opts.max
     configFile = opts.config
-    outputDir = opts.output
-    launch_go = opts.launch
+    launch = opts.launch
+    clobber = opts.clobber
 
     for myfilt in filterList:
         if filt == "all" or filt == myfilt :
@@ -109,22 +140,18 @@ if __name__ == "__main__":
                 else :
                     visitList = visitList[:-1]
                     print("Job for filter =",myfilt, " & visits =", visitList)
-                    if launch_go :
-                        submit(visitList, toWrite, configFile, myfilt)
+                    createSubmit(visitList, toWrite, configFile, myfilt, clobber,
+                           opts.rerun, launch)
                     toWrite = []
                     toWrite.append(line)
                     visitList = visit + "_"
                     imod = 1
-                        
+
             visitList = visitList[:-1]
-            print("Job for filter =",myfilt, " & visits =", visitList)
-            if launch_go :
-                submit(visitList, toWrite, configFile, myfilt)
-            
-    if launch_go == 0 :
+            print("Jobs for filter =",myfilt, " & visits =", visitList)
+            createSubmit(visitList, toWrite, configFile, myfilt, clobber,
+                   opts.rerun,launch)
+
+    if not launch :
         print("----------------------------------------------------------")
-        print(" Add option  --launch 1  to launch the jobs, if it looks OK")
-                        
-	
-	
-	
+        print(" Add option  --launch (or -L)  to launch the jobs, if it looks OK\n")
